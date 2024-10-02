bde26495c908ef6c459807abbd1815877e86c0f5
cuongphan <132993945+cuongphanse@users.noreply.github.com>
2024-10-02 22:04:08 +0700

    hotfix-header

--- CMMS/src/components/header/Header.jsx
+++ CMMS/src/components/header/Header.jsx
@@ -5,9 +5,19 @@ import SearchIcon from '../../assets/images/search.svg';
 import Select from "../selectDrop/select";
 import axios from 'axios';
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+import Wishlist from '../../assets/images/wishlist.svg';
+import Cart from '../../assets/images/cart.svg';
+import User from '../../assets/images/user.svg';
+import Button from '@mui/material/Button';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import ManageSearchOutlinedIcon from '@mui/icons-material/ManageSearchOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
 
 
 export default function Header(){
+
+    const [isOpenDropDown, setIsOpenDownDown] = useState(false);
 
     const [categories,setCategories] = useState([
         'Xi măng',
@@ -61,9 +71,43 @@ export default function Header(){
                             </div>
                         </div>
 
-                        <div className="col-sm-5">
-                            <div className="countryWrapper">
-                                <Select data={countryList} placeholder='Your location' icon ={<LocationOnOutlinedIcon style={{opacity:'0.5'}}/>}/>
+                        <div className="col-sm-5 d-flex justify-content-end">
+                            <div className="ml-auto d-flex align-items-center">
+                                <div className="countryWrapper">
+                                    <Select data={countryList} placeholder='Your location' icon ={<LocationOnOutlinedIcon style={{opacity:'0.5'}}/>}/>
+                                </div>
+
+                                <ul className="list list-inline mb-2 headerTabs">
+                                    <li className="list-inline-item">
+                                        <span>
+                                            <img src={Wishlist} alt="wishlist" />
+                                            <span className="badge bg-info rounded-circle">3</span>  
+                                               Wishlist
+                                        </span>
+                                    </li>
+                                    <li className="list-inline-item">
+                                        <span>
+                                            <img src={Cart} alt="wishlist" />
+                                            <span className="badge bg-info rounded-circle">3</span>  
+                                               Wishlist
+                                        </span>
+                                    </li>
+                                    <li className="list-inline-item">
+                                        <span onClick={() => setIsOpenDownDown(isOpenDropDown => !isOpenDropDown)}>
+                                            <img src={User} alt="wishlist" />
+                                                Account
+                                        </span>
+                                    {
+                                        isOpenDropDown!== false &&
+                                        <ul className="dropdownMenu">
+                                            <li><Button><PersonOutlineOutlinedIcon/> My Account</Button></li>
+                                            <li><Button><ManageSearchOutlinedIcon/> Order History</Button></li>
+                                            <li><Button><FavoriteBorderOutlinedIcon/> My Wishlist</Button></li>
+                                            <li><Button><LogoutOutlinedIcon/> Sign Out</Button></li>
+                                        </ul>
+                                    }
+                                    </li>
+                                </ul>
                             </div>
                         </div>
                     </div>
--- CMMS/src/components/selectDrop/select.jsx
+++ CMMS/src/components/selectDrop/select.jsx
@@ -10,6 +10,8 @@ export default function Select({data, placeholder, icon}){
     const [isOpenSelect, setIsOpenSelect] = useState(false);
     const [isSelect,setIsSelect] = useState(0);
     const [selectItem,setSelectItem] = useState(placeholder);
+    const [listData, setListData] = useState(data);
+    const [listData2, setListData2] = useState(data);
 
     function handleClick(){
         setIsOpenSelect(isOpenSelect => !isOpenSelect);
@@ -21,6 +23,18 @@ export default function Select({data, placeholder, icon}){
         setSelectItem(name);
     }
 
+    const filterList = (e) =>{
+        const keyword = e.target.value.toLowerCase();
+        
+        const list = listData2.filter((item) =>{
+            return item.toLowerCase().includes(keyword);
+        })
+
+        const list2 = list.filter((item,index) => list.indexOf(item) === index); //check duplicate
+
+        setListData(list2);
+    }
+
     return (
         <>
         <ClickAwayListener onClickAway={() => setIsOpenSelect(false)}>
@@ -28,7 +42,7 @@ export default function Select({data, placeholder, icon}){
             <div  className="selectDropWrapper cursor position-relative">
                 <div onClick={handleClick} className="handleClick">
                     {icon}
-                    <span className="openSelect" >{selectItem} 
+                    <span className="openSelect" >{selectItem.length>14 ? selectItem.substr(0,12) + '...' : selectItem} 
                         <KeyboardArrowDownIcon className="arrow"/>
                     </span>
                 </div>
@@ -37,12 +51,12 @@ export default function Select({data, placeholder, icon}){
                 
                     <div className="selectedDrop">
                         <div className="searchFlied">
-                            <input type="text" placeholder="Search here..."/>
+                            <input type="text" placeholder="Search here..." onChange={filterList}/>
                         </div>
                         <ul className="searchResult">
                             <li key={0} onClick={() => closeSelect(0,placeholder)} className={`${isSelect === 0 ? 'active' : ''}`}>{placeholder}</li>
                             {
-                                data.map((item,index) =>{
+                                listData.map((item,index) =>{
                                     return (
                                         <li key={index + 1} onClick={() => closeSelect(index +1,item)} className={`${isSelect === index+1 ? 'active' : ''}`}>{item}</li>
                                     );
